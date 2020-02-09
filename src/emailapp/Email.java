9a166486eff04de52324cc8f490e3855f12f53ca
tonie <tonie@tonie.com>
2020-02-09 17:06:43 +0100

    second Attempt

---
+++ src/emailapp/Email.java
@@ -0,0 +1,51 @@
+package emailapp;
+
+public class Email {
+
+	
+	private String Firstname;
+	private String Lastname;
+	private String Password;
+	private String Department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	//constructor to receive last and first name 
+	public Email(String Firstname, String Lastname) {
+		
+		
+		//defining the Class level to the Local Variable 
+		this.Firstname = Firstname;
+		
+		this.Lastname = Lastname;
+		
+		System.out.println("Email Created :" + this.Firstname + " " + this.Lastname );
+	}
+	
+	
+	
+	//ask for Department 
+	
+	
+	
+	//Generate Random Password
+	
+	
+	
+	//Set mailbox Capacity 
+	
+	
+	
+	//Set Alternate Email
+	
+	
+	
+	
+	//Change Password 
+	
+	
+	
+	
+	
+	
+}
